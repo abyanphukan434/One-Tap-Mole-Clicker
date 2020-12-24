2c9819987df7ef89b57fafe4d1793c3ff6f3429b
abyanphukan434 <72485127+abyanphukan434@users.noreply.github.com>
2020-12-24 06:55:32 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# One-Tap-Mole-Clicker
+# Web Mole Game
+
+Mole game with HTML, CSS, JavaScript. (Web-based)
+
+- Get all the **CSS** and **HTML** on the page. Have ten holes with a hungry mole in each. No JS done yet.
+- Make the moles show up and disappear (don't worry about the sad face / butt yet.) Don't make them clickable yet, just have them disappear and reappear after a random interval.
+- Make the hungry moles clickable. If you click on one, add 1 point to your score.
+- After 10 clicks, show the win screen.
+- Show the worm meter. As a player clicks on moles, show more and more of the worm.
+- Make one in ten of the moles that show up a royal mole. If a user clicks a royal mole, add 2 points to their score.
+- If a user doesn't click a mole in time, show a sad mole.
+- If a user does click a mole in time, show a fed mole.
+- After both sad and fed moles, show a mole butt.
+- You're done!
+
+Hope you like it!
---
+++ script.js
@@ -0,0 +1,140 @@
+// Max Base
+// https://github.com/BaseMax/WebMoleGame
+
+var score=0;
+var win=document.querySelector("#win");
+var container=document.querySelector("#container");
+var worm=container.querySelector("#worm");
+var holes=document.querySelectorAll(".hole");
+
+const TIME=2000;
+const TIME_OF_A_MOLE=5000;
+const TIME_SHOW_GONE=500;
+const TIME_SHOW_LEAVING=200;
+const TIME_SHOW_SADLY=2000;
+const TIME_SHOW_FED=1000;
+const TIME_SHOW_WORM=1000;
+var statusHoles=[];
+
+if(!Element.prototype.triggerEvent) {
+	Element.prototype.triggerEvent = function(eventName) {
+		var event;
+		if(document.createEvent) {
+			event = document.createEvent("HTMLEvents");
+			event.initEvent(eventName, true, true);
+		}
+		else {
+			event = document.createEventObject();
+			event.eventType = eventName;
+		}
+		event.eventName = eventName;
+		if(document.createEvent) {
+			this.dispatchEvent(event);
+		}
+		else {
+			this.fireEvent("on" + event.eventType, event);
+		}
+	};
+}
+function showHole() {
+	var random=Math.floor((Math.random() * 10) + 1)-1;
+	var hole=holes[random];
+	var image=hole.querySelector(".image");
+	if(image.classList.contains("gone")) {
+		image.classList.remove("gone");
+		image.classList.remove("sad");
+		image.classList.remove("fed");
+		image.classList.remove("king");
+		image.classList.add("hungry");
+		if(Math.random() >= 0.9) { // ~ 10%
+			image.classList.add("king");
+		}
+		statusHoles[random]["timer"]=setTimeout(function() {
+			statusHoles[random]={};
+			image.classList.add("sad");
+			image.classList.remove("hungry");
+			image.triggerEvent("mouseout");
+			setTimeout(function() {
+				image.classList.add("gone");
+				image.classList.remove("king");
+			}, TIME_SHOW_SADLY);
+		}, TIME_OF_A_MOLE);
+	}
+}
+
+function updateScore(value) {
+	score+=value;
+	if(score>10) {
+		score=10;
+	}
+	if(value > 0) {
+		console.log("SUCCESS / Score: "+score);
+	}
+	else {
+		console.log("FAIL / Score: "+score);
+	}
+	worm.style.width=(score*10)+"%";
+	if(score >= 10) {
+		setTimeout(function() {
+			container.classList.add("gone");
+			win.classList.remove("gone");
+		}, TIME_SHOW_WORM);
+	}
+}
+
+window.addEventListener("load", function() {
+	holes.forEach(function(hole) {
+		var id=parseInt(hole.getAttribute("data-id"));
+		var image=hole.querySelector(".image");
+		image.addEventListener("mouseover", function(e) {
+			if(e.target.classList.contains("hungry") && !e.target.classList.contains("sad")) {
+				container.style.cursor="url('images/cursor-worm.png'), auto";
+			}
+		});
+		image.addEventListener("mouseout", function(e) {
+			container.style.cursor="url('images/cursor.png'), auto";
+		});
+		image.addEventListener("click", function(e) {
+			id=parseInt(this.parentNode.parentNode.getAttribute("data-id"));
+			if(statusHoles[id-1] !== undefined) {
+				id=id-1;
+				if(statusHoles[id]["timer"]) {
+					clearTimeout(statusHoles[id]["timer"]);
+					statusHoles[id]={};
+					if(image.classList.contains("king")) {
+						updateScore(+2);
+					}
+					else {
+						updateScore(+1);
+					}
+					image.classList.remove("hungry");
+					image.classList.add("fed");
+					setTimeout(function() {
+						image.classList.add("gone");
+						image.classList.remove("fed");
+						image.classList.add("hungry");
+					}, TIME_SHOW_FED);
+				}
+				else {
+					// SKIP: your mole is not hungry, he is sad!
+				}
+			}
+			else {
+				// SKIP: game did not start yet!
+			}
+		});
+		setTimeout(function() {
+			image.classList.add("leaving");
+			image.classList.remove("hungry");
+			setTimeout(function() {
+				image.classList.add("gone");
+				image.classList.add("hungry");
+				image.classList.remove("leaving");
+				statusHoles.push({});
+			}, TIME_SHOW_GONE);
+		}, TIME_SHOW_LEAVING);
+	});
+});
+setTimeout(function() {
+	setInterval(showHole, 1+Math.random() * TIME);
+}, TIME_SHOW_GONE+TIME_SHOW_LEAVING);
